a0eda09366d6f1c3f061415376fe61600599a822
khushi <khushi.sali2002@gmail.com>
2023-11-12 19:50:31 +0530

    frontend basic

--- agri/smagri/variables.py
+++ agri/smagri/variables.py
@@ -1,10 +1,6 @@
 import os
-
-
 path = os.path.join(os.path.dirname(__file__), 'ds1.csv')
-
 import pandas as pd
-import matplotlib.pyplot as plt
 
 dataset = pd.read_csv(path)
 fert = list(dataset.Fertilizer)
@@ -21,10 +17,6 @@ dn = list(dataset.District_Name.unique())
 DN = {k: v for v, k in enumerate(dn)}
 sc = list(dataset.Soil_color.unique())
 SC = {k: v for v, k in enumerate(sc)}
-c = list(dataset.Crop.unique())
-C = {k: v for v, k in enumerate(c)}
-f = list(dataset.Fertilizer.unique())
-F = {k: v for v, k in enumerate(f)}
 
 Dist = {'Kolhapur':0,'Solapur':1,'Satara':2,'Sangli':3,'Pune':4}
 Soil = {'Black':0,'Red':1, 'MediumBrown': 2, 'DarkBrown': 3, 'LightBrown':4,'ReddishBrown':5}

7e9b5b11bf3589b4cac8adf538781df246d20b6f
khushi <khushi.sali2002@gmail.com>
2024-02-06 20:52:29 +0530

    vercel

--- agri/agri/settings.py
+++ agri/agri/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s3$)3jv!+$uo0)p-hyu&9ro1*%9-wnejms!k%ty5-4c#d^yro%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','smart-agri-rnyj.onrender.com']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 # Application definition
 
